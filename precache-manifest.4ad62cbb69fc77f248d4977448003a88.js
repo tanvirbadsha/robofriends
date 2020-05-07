24d9832167540f8c07a7ee4addf589b32d3cd40e
tanvirbadsha <57857925+tanvirbadsha@users.noreply.github.com>
2020-05-07 17:04:31 +0600

    Updates

---
+++ precache-manifest.4ad62cbb69fc77f248d4977448003a88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0198e955d04e38bcfbb6851398c52e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "df387c919d748cd9e273",
+    "url": "/robofriends/static/css/2.54a67088.chunk.css"
+  },
+  {
+    "revision": "19bbccc0827d80962f24",
+    "url": "/robofriends/static/css/main.ab015a26.chunk.css"
+  },
+  {
+    "revision": "df387c919d748cd9e273",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19bbccc0827d80962f24",
+    "url": "/robofriends/static/js/main.d41e0a07.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
